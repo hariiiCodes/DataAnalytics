cfddc1b1f77f6db9910e4aaea75eda4ba571699a
Sreehari Sunil <sreeharisunil2005@gmail.com>
2026-06-21 13:00:52 +0530

    Built Main menu Structure

--- MiniProjectProgress.py
+++ MiniProjectProgress.py
@@ -1,47 +1,75 @@
-students = []
+students=[]
 
-def add_student():
-    name = input("Enter the name of student: ")
-    scores = []
+def main():                                            #DECLARED MAIN FUNCTION FIRST FOR CHECKING THE STRUCTURE OF PROGRAM.
     while True:
-        score = input("Enter score (or 'done'): ")
-        if score.lower() == "done":
+        print("\n--- Grade Calculator ---")
+        print("1. Add Student")
+        print("2. View Students")
+        print("3. Class Report")
+        print("4. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":                              
+           # add_student()                             #COMMENTED FOR LATER USE.     
+           print("1")
+        elif choice == "2":
+           # view_students()                           #COMMENTED FOR LATER USE. 
+           print("2")
+        elif choice == "3":
+           # class_report()                            #COMMENTED FOR LATER USE.
+            print("3")
+        elif choice == "4":
+            print("Program Ended")
             break
-        score = float(score)
-        if 0 <= score <= 100:
-            scores.append(score)
         else:
-            print("Score must be between 0 and 100")
+            print("Invalid choice")
 
-    average = calculate_average(scores)
-    grade = get_letter_grade(average)
+main()
 
-    student = {
-        "name": name,
-        "scores": scores,
-        "average": average,
-        "grade": grade
-    }
 
-    students.append(student)
+# def add_student():
+#     name = input("Enter the name of student: ")
+#     scores = []
+#     while True:
+#         score = input("Enter score (or 'done'): ")
+#         if score.lower() == "done":
+#             break
+#         score = float(score)
+#         if 0 <= score <= 100:
+#             scores.append(score)
+#         else:
+#             print("Score must be between 0 and 100")
 
-    print(f"\nAdded {name}")
-    print(f"Average: {average:.2f}")
-    print(f"Grade: {grade}")
+#     average = calculate_average(scores)
+#     grade = get_letter_grade(average)
 
-def calculate_average(scores):
-    if len(scores) == 0:
-        return 0
-    return sum(scores) / len(scores)
+#     student = {
+#         "name": name,
+#         "scores": scores,
+#         "average": average,
+#         "grade": grade
+#     }
 
-def get_letter_grade(avg):
-    if avg >= 90:
-        return "A"
-    elif avg >= 80:
-        return "B"
-    elif avg >= 70:
-        return "C"
-    elif avg >= 60:
-        return "D"
-    else:
-        return "F"
+#     students.append(student)
+
+#     print(f"\nAdded {name}")
+#     print(f"Average: {average:.2f}")
+#     print(f"Grade: {grade}")
+
+# def calculate_average(scores):
+#     if len(scores) == 0:
+#         return 0
+#     return sum(scores) / len(scores)
+
+# def get_letter_grade(avg):
+#     if avg >= 90:
+#         return "A"
+#     elif avg >= 80:
+#         return "B"
+#     elif avg >= 70:
+#         return "C"
+#     elif avg >= 60:
+#         return "D"
+#     else:
+#         return "F"
