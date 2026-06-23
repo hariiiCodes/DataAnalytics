28043e7358256f66579ea90bcb8f51e0f5bc0ae2
Sreehari Sunil <sreeharisunil2005@gmail.com>
2026-06-24 00:48:10 +0530

    Updated HousingData

--- pd.py
+++ pd.py
@@ -7,5 +7,25 @@ import pandas as pd
 # print(s)
 # x=pd.Series([10,20,30],index=['a','b','c'])
 # print(s)
-df=pd.read_csv(r"C:\Users\admin\Downloads\player_stats.csv")
-print(df.head())
+csv_data=pd.read_csv("sample.csv",encoding="latin1")
+excel_data=pd.read_excel("sample.xlsx")
+json_data=pd.read_json("sample.json")
+
+
+print("CSV Successfull!")
+print(csv_data.tail())
+
+print("------------------------------------------------------------")
+
+
+print("EXCEL Successfull!")
+print(excel_data.head())
+
+print("------------------------------------------------------------")
+
+
+print("JSON Successfull!")
+print(json_data.head())
+
+
+
